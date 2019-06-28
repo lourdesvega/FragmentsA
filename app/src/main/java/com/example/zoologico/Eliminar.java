8dfbecf596c53acd6fe495aa47560081238cfe10
lourdesvega <39252591+lourdesvega@users.noreply.github.com>
2019-06-27 20:28:48 -0500

    first commit

---
+++ app/src/main/java/com/example/zoologico/Eliminar.java
@@ -0,0 +1,35 @@
+package com.example.zoologico;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Eliminar.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class Eliminar extends Fragment {
+
+   // private OnFragmentInteractionListener mListener;
+
+    public Eliminar() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_eliminar, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/zoologico/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.zoologico;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.View;
+
+import androidx.core.view.GravityCompat;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setItemIconTintList(null);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.crearM) {
+            setTitle("Fragmento Crear");
+            Crear fragmento3 = new Crear();
+            androidx.fragment.app.FragmentTransaction transaction= getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.contenedor,fragmento3,"fragmento3");
+            transaction.commit();
+
+        } else if (id == R.id.editarM) {
+            setTitle("Fragmento Editar");
+            Modificar fragmento4 = new Modificar();
+            androidx.fragment.app.FragmentTransaction transaction= getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.contenedor,fragmento4,"fragmento4");
+            transaction.commit();
+
+        } else if (id == R.id.eliminarM) {
+            setTitle("Fragmento Eliminar");
+            Eliminar fragmento5 = new Eliminar();
+            androidx.fragment.app.FragmentTransaction transaction= getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.contenedor,fragmento5,"fragmento5");
+            transaction.commit();
+
+        } else if (id == R.id.listarM) {
+            setTitle("Fragmento Listar");
+            Listar fragmento6 = new Listar();
+            androidx.fragment.app.FragmentTransaction transaction= getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.contenedor,fragmento6,"fragmento6");
+            transaction.commit();
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
